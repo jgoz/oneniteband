454ea8a4bca8cb04d8e2a655243a75b71ca352de
Johnny Gozde <johnny@jgoz.net>
2010-11-17 13:54:12 -0700

    Added copyright to footer, using current date's year.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from datetime import datetime
 from helpers import RestFlask, templated
 
 app = RestFlask(__name__)
+
+@app.context_processor
+def inject_now():
+    return dict(now=datetime.now())
 
 @app.get('/')
 @templated()
