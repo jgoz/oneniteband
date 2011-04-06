5fd37930b3fa803a3adb234c7b1cbf5d3b02bafa
jgoz <johnny@jgoz.net>
2011-04-05 22:24:11 -0700

    Switch to lean, custom modernizr with a small head.js extension for detecting client width.

--- Rakefile
+++ Rakefile
@@ -10,3 +10,8 @@ task :png do
   system "pngnq -n 256 -s 1 images/*-o.png"
   system "find images/ -name '*-nq8.png' -print0 | xargs -P1 -0 -n1 pngout"
 end
+
+desc 'Minify modified head.js and append to modernizr. Muahahahaaaaa.'
+task :modjs do
+  system "curl --data-urlencode js_code@js/head.mod.js http://marijnhaverbeke.nl/uglifyjs | cat js/libs/modernizr.min.js - > js/libs/headmod.min.js"
+end
---
+++ js/head.mod.js
@@ -0,0 +1,43 @@
+/**
+  Head JS    The only script in your <HEAD>
+  Copyright  Tero Piirainen (tipiirai)
+  License    MIT / http://bit.ly/mit-license
+  Version    0.9
+  
+  http://headjs.com
+*/
+(function(doc) {
+
+  var html = doc.documentElement,
+      conf = { screens: [320, 480, 640, 768, 1024, 1280, 1440, 1680, 1920] },
+      klass = [];
+
+  function each(arr, fn) {  
+    for (var i = 0; i < arr.length; i++) {
+      fn.call(arr, arr[i], i);
+    }
+  }
+
+  // screen resolution: w-100, lt-480, lt-1024 ...
+  function screenSize() {
+    var w = window.outerWidth || html.clientWidth;
+
+    // remove earlier widths
+    html.className = html.className.replace(/ (w|lt|gt)-\d+/g, "");
+
+    // add new ones
+    klass.push("w-" + Math.round(w / 100) * 100);
+
+    each(conf.screens, function(width) {
+      if (w <= width) { klass.push("lt-" + width); } 
+      if (w > width) { klass.push("gt-" + width); } 
+    });
+
+    html.className += ' ' + klass.join(' ');
+    klass = [];
+  }
+
+  screenSize();    
+  window.onresize = screenSize;
+})(document);
+
